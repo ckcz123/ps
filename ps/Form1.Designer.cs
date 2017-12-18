454451cac4b4861800967feeff7bf224ef564431
ckcz123 <ckcz123@126.com>
2017-12-18 21:53:39 +0800

    first commit

---
+++ ps/Form1.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace ps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(56, 50);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(168, 395);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(168, 395);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(552, 50);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(168, 395);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(168, 395);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(245, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "加载图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(245, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "新增一行";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(245, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "保存图片";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(445, 88);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "加载图片";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(445, 155);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "新增一行";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(445, 224);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "保存图片";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.BackgroundImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(369, 153);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(366, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "剪切板";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(243, 333);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 12);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "使用说明：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 355);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(293, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "在一边选中某个位置图标后按Ctrl+C将复制到剪切板。";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(243, 379);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(257, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "另一边选中需要的位置Ctrl+V即可粘贴并覆盖。";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(243, 403);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(197, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Ctrl+Z可回退到上一步粘贴操作前。";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(243, 426);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(239, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "本工具只支持32x32像素的操作，敬请谅解。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(807, 506);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PS工具 By 艾之葵";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ ps/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Bitmap bitmap1, bitmap2, lbitmap1, lbitmap2;
+        private Bitmap picture1, picture2;
+        private Graphics graphics1, graphics2;
+        string directory1 = null, filename1 = null, directory2 = null, filename2 = null;
+        private string lastaction = null;
+        private int lx=-1, ly=-1, rx=-1, ry=-1;
+
+        private bool loadImage(ref string directory, ref string filename, ref Bitmap bitmap)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "PNG图片(*.png)|*.png";
+            if (string.IsNullOrEmpty(directory) || string.IsNullOrEmpty(directory))
+            {
+                dialog.InitialDirectory = Directory.GetCurrentDirectory();
+            }
+            else
+            {
+                dialog.InitialDirectory = directory1;
+                dialog.FileName = filename1;
+            }
+            dialog.AddExtension = true;
+            dialog.DefaultExt = ".png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Bitmap _bitmap = (Bitmap)Image.FromFile(dialog.FileName);
+                    if (_bitmap.Width % 32 != 0 || _bitmap.Height % 32 != 0)
+                    {
+                        MessageBox.Show("目标图片的宽或高不是32的倍数！", "加载失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return false;
+                    }
+                    bitmap = _bitmap;
+                    directory = Path.GetDirectoryName(dialog.FileName);
+                    filename = Path.GetFileName(dialog.FileName);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("加载失败！", "加载失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            return false;
+        }
+
+        private Bitmap cloneBitmap(Bitmap bitmap)
+        {
+            return bitmap.Clone(new Rectangle(0, 0, bitmap.Width, bitmap.Height), bitmap.PixelFormat);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (loadImage(ref directory1, ref filename1, ref bitmap1))
+            {
+                lbitmap1 = cloneBitmap(bitmap1);
+                picture1 = cloneBitmap(bitmap1);
+                graphics1 = Graphics.FromImage(picture1);
+                lx = ly = rx = ry = -1;
+                pictureBox1.Image = picture1;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (loadImage(ref directory2, ref filename2, ref bitmap2))
+            {
+                lbitmap2 = cloneBitmap(bitmap2);
+                picture2 = cloneBitmap(bitmap2);
+                graphics2 = Graphics.FromImage(picture2);
+                lx = ly = rx = ry = -1;
+                pictureBox2.Image = picture2;
+            }
+        }
+
+        private void drawBorder()
+        {
+            resetImage();
+            Pen pen = new Pen(Color.Crimson, 3);
+            pen.Alignment = PenAlignment.Inset;
+            if (lx >= 0 && ly >= 0)
+            {
+                graphics1.DrawRectangle(pen, 32 * lx, 32 * ly, 32, 32);
+                pictureBox1.Image = picture1;
+            }
+            if (rx >= 0 && ry >= 0)
+            {
+                graphics2.DrawRectangle(pen, 32 * rx, 32 * ry, 32, 32);
+                pictureBox2.Image = picture2;
+            }
+            pen.Dispose();
+        }
+
+        private void resetImage()
+        {
+            if (bitmap1 != null)
+            {
+                picture1 = cloneBitmap(bitmap1);
+                graphics1 = Graphics.FromImage(picture1);
+                pictureBox1.Image = picture1;
+            }
+            if (bitmap2 != null)
+            {
+                picture2 = cloneBitmap(bitmap2);
+                graphics2 = Graphics.FromImage(picture2);
+                pictureBox2.Image = picture2;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (picture1 == null)
+            {
+                MessageBox.Show("没有图片！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            lbitmap1 = cloneBitmap(bitmap1);
+            Bitmap nBitmap = new Bitmap(bitmap1.Width, bitmap1.Height + 32);
+            Graphics graphics = Graphics.FromImage(nBitmap);
+            graphics.DrawImage(bitmap1, 0, 0);
+            graphics.Dispose();
+            bitmap1 = nBitmap;
+            drawBorder();
+            lastaction = "add1";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (picture2 == null)
+            {
+                MessageBox.Show("没有图片！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            lbitmap2 = cloneBitmap(bitmap2);
+            Bitmap nBitmap = new Bitmap(bitmap2.Width, bitmap2.Height + 32);
+            Graphics graphics = Graphics.FromImage(nBitmap);
+            graphics.DrawImage(bitmap2, 0, 0);
+            graphics.Dispose();
+            bitmap2 = nBitmap;
+            drawBorder();
+            lastaction = "add2";
+        }
+
+        private bool saveImage(ref string directory, ref string filename, Bitmap bitmap)
+        {
+            if (bitmap == null)
+            {
+                MessageBox.Show("没有图片！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            SaveFileDialog dialog=new SaveFileDialog();
+            dialog.InitialDirectory = directory;
+            dialog.FileName = filename;
+            dialog.Filter = "PNG图片(*.png)|*.png";
+            dialog.AddExtension = true;
+            dialog.DefaultExt = ".png";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    bitmap.Save(dialog.FileName);
+                    directory = Path.GetDirectoryName(dialog.FileName);
+                    filename = Path.GetFileName(dialog.FileName);
+                    MessageBox.Show("图片已保存至" + dialog.FileName, "保存成功！", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                    return true;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("保存失败！", "保存失败", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+               
+            }
+            return false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (saveImage(ref directory1, ref filename1, bitmap1))
+            {
+                lbitmap1 = cloneBitmap(bitmap1);
+                lastaction = null;
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (saveImage(ref directory2, ref filename2, bitmap2))
+            {
+                lbitmap2 = cloneBitmap(bitmap2);
+                lastaction = null;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (bitmap1 == null) return;
+            rx = ry = -1;
+            lx = ((MouseEventArgs)e).X/32;
+            ly = ((MouseEventArgs)e).Y/32;
+            drawBorder();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            if (bitmap2 == null) return;
+            lx = ly = -1;
+            rx = ((MouseEventArgs)e).X / 32;
+            ry = ((MouseEventArgs)e).Y / 32;
+            drawBorder();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.C && e.Control)
+            {
+                // 复制
+                if (lx >= 0 && ly >= 0)
+                {
+                    
+                }
+            }
+        }
+
+
+    }
+}
